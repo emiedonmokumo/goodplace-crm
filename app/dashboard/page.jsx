a5b80369210dd36483dfa01558f75f47b39d40e9
Emie-Trialer <emiedonmukumo@gmail.com>
2024-09-07 14:03:12 +0100

    Added Signup and Login

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -4,7 +4,7 @@ const page = () => {
   return (
     <div>
       <h1 className="text-4xl font-bold text-center">Welcome to your Dashboard</h1>
-      <h4>Dashboard page in progress...</h4>
+      <h4>Dashboard page in progress..</h4>
     </div>
   )
 }
